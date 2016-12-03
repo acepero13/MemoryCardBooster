c051dab09cec1041b76b11369a3b298ae7174cd7
acepero13 <alvaro@egluu.com>
2016-12-03 20:34:18 +0100

    First import

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alvaro on 12/3/16.
+ */
+jQuery( document ).ready(function() {
+    // Handler for .ready() called.
+    jQuery("#card").flip({
+        axis: 'x',
+        trigger: 'click'
+    });
+});
