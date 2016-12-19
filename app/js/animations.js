0e458e358ceb84641029276af3952d32d945a99b
acepero13 <alvaro@egluu.com>
2016-12-19 12:36:34 +0100

    Keyboard controls

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,10 +1,28 @@
 /**
  * Created by alvaro on 12/3/16.
  */
+var visible_side = 'main';
+function flipCard() {
+    if(visible_side == 'main'){
+        jQuery("#card").flip(true);
+        visible_side = 'secondary';
+    }else{
+        jQuery("#card").flip(false);
+        visible_side = 'main';
+    }
+}
+
+function flipToMain() {
+    if(visible_side == 'secondary'){
+        jQuery("#card").flip(false);
+        visible_side = 'main';
+    }
+}
 jQuery( document ).ready(function() {
     // Handler for .ready() called.
     jQuery("#card").flip({
         axis: 'x',
-        trigger: 'click' // use manual
+        trigger: 'manual' // use manual
     });
+
 });
--- app/modules/card/controllers/Card.js
+++ app/modules/card/controllers/Card.js
@@ -5,7 +5,27 @@ var b = require('buffer');
 cardModule.controller('CardController', function ($rootScope, $scope, CardIteratorRespository) {
       $scope.card = {primary_card: "Hallo", secondary_card: "Hola"};
       $rootScope.card = $scope.card;
+      const {globalShortcut} = require('electron').remote;
+
+      globalShortcut.register('Left', function () {
+          $scope.getPreviousCard();
+      });
+
+    globalShortcut.register('Right', function () {
+        $scope.getNextCard();
+    });
+
+    globalShortcut.register('Space', function () {
+        flipCard();
+    });
+
+    globalShortcut.register('Space', function () {
+        $scope.getNextCard();
+    });
+
+
       $scope.getNextCard = function() {
+          flipToMain();
           CardIteratorRespository.getNextCard().then(function (card) {
               $scope.card = card;
               $scope.card.image =$scope.card.image.toString();
@@ -16,6 +36,7 @@ cardModule.controller('CardController', function ($rootScope, $scope, CardIterat
       };
 
       $scope.getPreviousCard = function () {
+          flipToMain();
           CardIteratorRespository.getPreviousCard().then(function (card) {
               $scope.card = card;
               $rootScope.card = $scope.card;
